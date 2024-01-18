18a0cacb59a1b6778d974d9bb31a9b60322a744f
fikireab-m <fikireab.meku@gmail.com>
2024-01-18 22:58:31 +0300

    Title change

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Donation Wave',
+      title: 'CharityWave',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Donation Wave'),
+      home: const MyHomePage(title: 'CharityWave'),
     );
   }
 }
