4e3c68ba692718698b15e86a2190c8baac32696a
fikireab-m <fikireab.meku@gmail.com>
2024-01-20 16:21:41 +0300

    loading required assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
 import 'package:charitywave/screens/mapview/mapview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CharityWaveApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CharityWaveApp extends StatelessWidget {
+  const CharityWaveApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CharityWave',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: const MapViewScreen(),
+    final s = MediaQuery.of(context).size;
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      designSize: Size(s.width, s.height),
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'CharityWave',
+          theme: ThemeData(
+            colorScheme:
+                ColorScheme.fromSeed(seedColor: const Color(0xFF2F67F1)),
+            useMaterial3: true,
+          ),
+          home: child,
+        );
+      },
+      child: const MapViewScreen(),
     );
   }
 }
