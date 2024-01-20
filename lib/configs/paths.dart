2caed9b99686eeba7c17ba7d367ca7dd73563e59
fikireab-m <fikireab.meku@gmail.com>
2024-01-20 13:08:56 +0300

    map icons

---
+++ lib/configs/paths.dart
@@ -0,0 +1,7 @@
+class AppIcons {
+  static const String locationPin = "assets/icons/location_pin_outlined.png";
+  static const String bigRedPin = "assets/icons/big_red_pin.png";
+  static const String redPin = "assets/icons/red_pin.png";
+  static const String greenPin = "assets/icons/green_pin.png";
+  static const String lightGreenPin = "assets/icons/light_green_pin.png";
+}
